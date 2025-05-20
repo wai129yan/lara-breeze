4d41ee876c91855e8cc40e1b48bb9431fbe24647
wai129yan <waiyan.msc.yu12@gmail.com>
2025-05-20 11:13:29 +0900

    phone-row-add

--- resources/views/components/hello.blade.php
+++ resources/views/components/hello.blade.php
@@ -5,7 +5,6 @@
         {{ $hello ?? $slot }}</p>
 </div> --}}
 @props(['value'])
-
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-rose-700 dark:text-gray-300']) }}>
     {{ $value ?? $slot }}
 </label>
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -23,17 +23,30 @@
                 required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
+
         <div>
-            <x-hello hello="Hello">hello</x-hello>
-            <x-hello class="dark:text-green-300">Hi</x-hello>
-            <x-hello :value="'hello welcome'" />
+            <x-input-label for="phone" :value="__('Phone')" />
+            <x-text-input id="phone" name="phone" type="text" class="mt-1 block w-full" :value="old('phone', $user->phone)"
+                required autofocus autocomplete="phone" />
+            <x-input-error class="mt-2" :messages="$errors->get('phone')" />
         </div>
+
+
+        {{--
         <div>
+            <x-hello>hello</x-hello>
+            <x-hello class="dark:text-yellow-300">Hi</x-hello>
+            {{-- <x-hello class="text-green-500 text-2xl" :value="('hello welcome')"></x-hello> --}}
+              {{-- <x-hello class="text-green-500 text-2xl" value="('hello welcome')" />
+        </div> --}}
+
+
+        {{-- <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)"
                 required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
-        </div>
+        </div> --}}
 
         <div>
             <x-input-label for="email" :value="__('Email')" />
